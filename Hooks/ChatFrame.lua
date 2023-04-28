a22a80952e5443bdc22eb559f5a40530ce6d57b6
Nicholas Drzewiecki <drzewieckinicholas@gmail.com>
2023-04-28 15:05:09 -0700

    refactor: Added getChatFrames() method to Utils.lua and implemented where appropriate

--- Hooks/ChatFrame.lua
+++ Hooks/ChatFrame.lua
@@ -13,11 +13,7 @@ local function OnChatFrameHide(chatFrame)
 end
 
 function ChatFramePlus.Hooks.ChatFrame:Init()
-	local chatFrames = {}
-
-	for i = 1, NUM_CHAT_WINDOWS do
-		chatFrames[i] = _G["ChatFrame" .. i]
-	end
+	local chatFrames = Addon.Utils.getChatFrames()
 
 	for _, chatFrame in ipairs(chatFrames) do
 		ChatFramePlus:SecureHookScript(chatFrame, "OnShow", OnChatFrameShow)
